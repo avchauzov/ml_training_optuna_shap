improved tests; fixed metric calculation; changed patience to float value

src/automl/optimization.py
@@ -7,7 +7,7 @@ import warnings
 import numpy as np
 import optuna
 
-from src.automl.metrics import METRIC_FUNCTIONS
+from src.automl.metric_functions import METRIC_FUNCTIONS
 from src.automl.study import optuna_hyperparameter_optimization
 from src.models.models import MODELS
 from src.scripts.feature_selection import feature_selection
@@ -16,7 +16,6 @@ from src.utils.tasks import TASKS
 
 warnings.filterwarnings('always')
 optuna.logging.set_verbosity(optuna.logging.CRITICAL)
-
 
 
 def validate_task_model_metric(task_name, model_name, metric_name):
@@ -42,7 +41,7 @@ def validate_task_model_metric(task_name, model_name, metric_name):
 		raise ValueError(f'Error: Unknown metric name: {metric_name}')
 
 
-def find_best_model(task_name, model_name, metric_name, x_data, y_data, weight_data, cv, n_trials_long, n_trials_short, patience, n_jobs, drop_rate=0.5, min_columns_to_keep=8, test_mode=False):
+def find_best_model(task_name, model_name, metric_name, x_data, y_data, weight_data, cv, n_trials_long, n_trials_short, patience=0.1, n_jobs=-1, drop_rate=0.5, min_columns_to_keep=8, test_mode=False):
 	"""
 	Automates the process of model selection, hyperparameter optimization, and feature selection.
 
@@ -144,7 +143,7 @@ def optimize_hyperparameters(data, hyperparameters, cv, trials, patience, trial_
 		Exception: If no valid hyperparameters are found during optimization.
 
 	"""
-	best_hyperparameters = optuna_hyperparameter_optimization(data, cv, hyperparameters, trials, patience, trial_type, n_jobs, task_name, model_name, metric_name, test_mode)
+	best_hyperparameters = optuna_hyperparameter_optimization(data, cv, hyperparameters, trials, int(np.ceil(patience * trials)), trial_type, n_jobs, task_name, model_name, metric_name, test_mode)
 	
 	if not best_hyperparameters:
 		raise Exception('Hyperparameter optimization did not find any valid hyperparameters. Please check your input data, hyperparameter search space, or "n_trials" value.')

src/utils/custom_metrics.py
@@ -5,24 +5,24 @@ This module contains a function to calculate Symmetrical Mean Absolute Percentag
 import numpy as np
 
 
-def calculate_symmetrical_mape(y_true, y_pred, sample_weights):
+def calculate_symmetrical_mape(y_true, y_pred, sample_weight):
 	"""
 	Calculate Symmetrical Mean Absolute Percentage Error (sMAPE).
 
 	Args:
 		y_true (array-like): True target values.
 		y_pred (array-like): Predicted values.
-		sample_weights (array-like): Sample weights for each data point.
+		sample_weight (array-like): Sample weights for each data point.
 
 	Returns:
 		float: Symmetrical Mean Absolute Percentage Error (sMAPE).
 	"""
 	# Calculate the absolute errors and apply sample weights
-	absolute_errors = sample_weights * np.abs(y_true - y_pred)
+	absolute_errors = sample_weight * np.abs(y_true - y_pred)
 	
 	# Calculate the absolute sum of true and predicted values, applying sample weights
 	# 1e-7 is added to avoid division by zero
-	sum_absolute_values = sample_weights * (np.abs(y_true) + np.abs(y_pred)) + 1e-7
+	sum_absolute_values = sample_weight * (np.abs(y_true) + np.abs(y_pred)) + 1e-7
 	
 	# Calculate the sMAPE
 	sMAPE = np.nanmean(2.0 * absolute_errors / sum_absolute_values)

src/utils/functions.py
@@ -2,10 +2,7 @@
 This module contains functions for calculating test errors and metrics, as well as updating dictionaries.
 """
 
-import numpy as np
-from sklearn.preprocessing import OneHotEncoder
-
-from src.automl.metrics import METRIC_FUNCTIONS
+from src.automl.metric_functions import METRIC_FUNCTIONS
 
 
 def calculate_test_error(data, model, metric_name, task_name):
@@ -46,8 +43,8 @@ def calculate_error(data, task_name, metric_name):
 	"""
 	y_true, y_pred, sample_weight = data
 	
-	if task_name not in ['regression'] and y_true.ndim == 1:
-		y_true = OneHotEncoder(sparse_output=False).fit_transform(np.array(y_true).reshape(-1, 1))
+	if task_name in ['classification_binary']:
+		y_pred = y_pred[:, 1]
 	
 	# Calculate metric using calculate_metric function
 	return calculate_metric([y_true, y_pred, sample_weight], metric_name)